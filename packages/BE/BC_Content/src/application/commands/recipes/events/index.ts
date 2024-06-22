ab3f2fb60c120d077b4760f4490eef7a3ac1dfcb
dgmetje1 <dgmetje1@gmail.com>
2024-06-22 12:31:48 +0200

    refact: move eventHandlers import to a separate file

--- packages/BE/BC_Content/src/application/commands/recipes/events/index.ts
+++ packages/BE/BC_Content/src/application/commands/recipes/events/index.ts
@@ -1,7 +1,15 @@
 import { IHandle } from "@rcp-and-plan/commons";
 
 import { SaveEntryOnRecipeIngredientAdded } from "./RecipeIngredientAdded/SaveEntryOnRecipeIngredientAdded";
+import { SaveEntryOnRecipeKitchenwareAdded } from "./RecipeKitchenwareAdded/SaveEntryOnRecipeKitchenwareAdded";
+import { SaveEntryOnRecipeStepAdded } from "./RecipeStepAdded/SaveEntryOnRecipeStepAdded";
 
-const events: Array<typeof IHandle> = [SaveEntryOnRecipeIngredientAdded];
+type IHandleConstructor = new (...args: unknown[]) => IHandle;
+
+const events: Array<IHandleConstructor> = [
+  SaveEntryOnRecipeIngredientAdded,
+  SaveEntryOnRecipeKitchenwareAdded,
+  SaveEntryOnRecipeStepAdded,
+];
 
 export default events;
