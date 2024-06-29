3e8e826e9bcfff047ab65458dbb566396bb5d8e6
dgmetje1 <dgmetje1@gmail.com>
2024-06-29 17:26:35 +0200

    chore: improve linting of imports inside BE Services

--- packages/BE/BC_Social/.eslintrc.cjs
+++ packages/BE/BC_Social/.eslintrc.cjs
@@ -24,5 +24,121 @@ module.exports = {
         "@typescript-eslint/no-floating-promises": "warn",
       },
     },
+    {
+      files: ["./src/services/**/*.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@domain/*"],
+                message: "Unnallowed access to Domain Layer",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      files: ["./src/domain/**/*.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@application/*", "@services/*"],
+                message: "Unnallowed access from Domain Layer",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      files: ["./src/domain/**/*.ts"],
+      excludedFiles: ["./src/domain/**/*Repository.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@infrastructure/*"],
+                message: "Unnallowed access from Domain Layer",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      files: ["./src/infrastructure/**/*.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@services/*"],
+                message: "Unnallowed access to Service Layer",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      files: ["./src/application/**/*.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@services/*"],
+                message: "Unnallowed access to Service Layer",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      files: ["./src/application/**/*.ts"],
+      excludedFiles: ["./src/application/**/*Queries.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@infrastructure/*"],
+                message: "Unnallowed access to Infrastructure Layer",
+              },
+            ],
+          },
+        ],
+      },
+    },
   ],
 };
